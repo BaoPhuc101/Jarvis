0dc20e4507fda960eb804cbffb06c684a63108b6
BaoPhuc101 <22110200@student.hcmute.edu.vn>
2024-11-04 14:31:08 +0700

    Jarvis - Day 6

--- engine/commands.py
+++ engine/commands.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init("sapi5")
@@ -9,4 +10,26 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("I love Vietnam, my name is Le Hoang Bao Phuc")
+def take_command():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print("Recognizing")
+        query = r.recognize_google(audio, language="vi")
+        print(f"User said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text = take_command()
+
+# speak("I love Vietnam, my name is Le Hoang Bao Phuc")
+speak(text)
